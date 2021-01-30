dea65d22043bd60fc327bf6a6aba0f3a1f28a088
JonTDean <JonathanTomDean@Gmail.com>
2021-01-30 18:17:52 -0500

    Beginning

---
+++ EmployeeDues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeDues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Employee
+    {
+
+    }
+}
